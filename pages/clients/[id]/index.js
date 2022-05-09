aba1f1f4297034f59c5cce8d279608511ccd4f6f
hossein-ghadirzadeh <hossein.ghadirzadeh@gmail.com>
2022-05-09 16:35:48 +0430

    feat: add nested dynamic routes & paths

---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+const ClientProjectsPage = () => {
+  const router = useRouter();
+
+  console.log(router.query);
+
+  return (
+    <div>
+      <h1>The Projects of a given client</h1>
+    </div>
+  );
+};
+
+export default ClientProjectsPage;
