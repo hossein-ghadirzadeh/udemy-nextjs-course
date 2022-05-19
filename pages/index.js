fff99bf1872b6a279cbaa384af1936361c5a901e
hossein-ghadirzadeh <hossein.ghadirzadeh@gmail.com>
2022-05-19 10:59:09 +0430

    feat: adding and styling components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
 
 const HomePage = () => {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>The Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 };
